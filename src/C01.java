f04c3888246807db17383e419b4c4e1e9054085e
teasolves <pisipisi@gmail.com>
2023-04-06 09:20:28 +0300

    merge

--- src/C01.java
+++ src/C01.java
@@ -1,17 +1,14 @@
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
-<<<<<<< HEAD
+
     main branch değişiklik 1. satır
     main branck 2. satır
     
-=======
+
 
         ahmet branch
-        
 
-
->>>>>>> ahmet
     }
 }
 z
--- src/C02.java
+++ src/C02.java
@@ -1,7 +1,7 @@
 public class C02 {
     public static void main(String[] args) {
         System.out.println("C2");
-        ortak class
+    
     
     }
 }
