b26f6a25c207e2a2976832dc7f0a3e7fea5721f9
teasolves <pisipisi@gmail.com>
2023-04-06 08:50:15 +0300

    main branch değişiklik

--- src/C01.java
+++ src/C01.java
@@ -1,5 +1,8 @@
 public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+    main branch değişiklik 1. satır
+    main branck 2. satır
+    
     }
 }
